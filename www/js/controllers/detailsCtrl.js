c5ade1e652a8de17f6738c704ef52fea4548a99d
nick <nicksugar@outlook.com>
2016-07-12 18:50:31 -0600

    added trip detail html and related controller data

--- www/js/controllers/detailsCtrl.js
+++ www/js/controllers/detailsCtrl.js
@@ -1,4 +1,20 @@
 angular.module('triptrader')
 .controller('detailsCtrl', function($scope){
-	
+	$scope.trips=[
+		{
+			base: 'SEA',
+			dates: '07Jul16 - 11Jul16',
+			tripType: 'PM FA',
+			reportTime:'1800',
+			releaseTime:'1800',
+			destination: 'KOA/PDX/ATL',
+			layoverLength:'18/9/1150',
+			position:'FB',
+			tfp:'4.6',
+			bonus:'$100',
+			days:'2',
+			postedBy:'RioJane',
+			comment:'Come alone or the deal is off!!!'
+		}
+	];
 });
